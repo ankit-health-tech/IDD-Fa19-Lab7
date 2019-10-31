4a2842564db89fc9a244e90762fc0750d2e144c8
Ankit <ankitmalhan.iitd@gmail.com>
2019-10-31 08:13:53 -0700

    Minor Tweaks on debugging

--- pictureServer.js
+++ pictureServer.js
@@ -22,12 +22,13 @@ line input.
 var express = require('express'); // web server application
 var app = express(); // webapp
 var http = require('http').Server(app); // connects http library to server
+var serverPort  = 8000;
 var io = require('socket.io')(http); // connect websocket library to server
-var serverPort = 8000;
 var SerialPort = require('serialport'); // serial library
 var Readline = SerialPort.parsers.Readline; // read serial data as lines
 //-- Addition:
 var NodeWebcam = require( "node-webcam" );// load the webcam module
+var nodemailer = require('nodemailer');
 
 //---------------------- WEBAPP SERVER SETUP ---------------------------------//
 // use express to create the simple webapp
@@ -36,13 +37,13 @@ app.use(express.static('public')); // find pages in public directory
 // check to make sure that the user provides the serial port for the Arduino
 // when running the server
 if (!process.argv[2]) {
-  console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
-  process.exit(1);
+	console.error('Usage: node ' + process.argv[1] + ' SERIAL_PORT');
+	process.exit(1);
 }
 
 // start the server and say what port it is on
 http.listen(serverPort, function() {
-  console.log('listening on *:%s', serverPort);
+	console.log('listening on *:%s', serverPort);
 });
 //----------------------------------------------------------------------------//
 
@@ -50,26 +51,26 @@ http.listen(serverPort, function() {
 //----------------------------WEBCAM SETUP------------------------------------//
 //Default options
 var opts = { //These Options define how the webcam is operated.
-    //Picture related
-    width: 1280, //size
-    height: 720,
-    quality: 100,
-    //Delay to take shot
-    delay: 0,
-    //Save shots in memory
-    saveShots: true,
-    // [jpeg, png] support varies
-    // Webcam.OutputTypes
-    output: "jpeg",
-    //Which camera to use
-    //Use Webcam.list() for results
-    //false for default device
-    device: false,
-    // [location, buffer, base64]
-    // Webcam.CallbackReturnTypes
-    callbackReturn: "location",
-    //Logging
-    verbose: false
+	//Picture related
+	width: 1280, //size
+	height: 720,
+	quality: 100,
+	//Delay to take shot
+	delay: 0,
+	//Save shots in memory
+	saveShots: true,
+	// [jpeg, png] support varies
+	// Webcam.OutputTypes
+	output: "jpeg",
+	//Which camera to use
+	//Use Webcam.list() for results
+	//false for default device
+	device: false,
+	// [location, buffer, base64]
+	// Webcam.CallbackReturnTypes
+	callbackReturn: "location",
+	//Logging
+	verbose: false
 };
 var Webcam = NodeWebcam.create( opts ); //starting up the webcam
 //----------------------------------------------------------------------------//
@@ -80,14 +81,14 @@ var Webcam = NodeWebcam.create( opts ); //starting up the webcam
 // start the serial port connection and read on newlines
 const serial = new SerialPort(process.argv[2], {});
 const parser = new Readline({
-  delimiter: '\r\n'
+	delimiter: '\r\n'
 });
 
 // Read data that is available on the serial port and send it to the websocket
 serial.pipe(parser);
 parser.on('data', function(data) {
-  console.log('Data:', data);
-  io.emit('server-msg', data);
+	console.log('Data:', data);
+	io.emit('server-msg', data);
 });
 //----------------------------------------------------------------------------//
 
@@ -96,6 +97,7 @@ parser.on('data', function(data) {
 // this is the websocket event handler and say if someone connects
 // as long as someone is connected, listen for messages
 io.on('connect', function(socket) {
+<<<<<<< HEAD
   console.log('a user connected');
 
   // if you get the 'ledON' msg, send an 'H' to the Arduino
@@ -161,5 +163,71 @@ io.on('connect', function(socket) {
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	});
+=======
+	console.log('a user connected');
+
+	// if you get the 'ledON' msg, send an 'H' to the Arduino
+	socket.on('ledON', function() {
+		console.log('ledON');
+		serial.write('H');
+	});
+
+	// if you get the 'ledOFF' msg, send an 'L' to the Arduino
+	socket.on('ledOFF', function() {
+		console.log('ledOFF');
+		serial.write('L');
+	});
+
+	//-- Addition: This function is called when the client clicks on the `Take a picture` button.
+	socket.on('takePicture', function() {
+		/// First, we create a name for the new picture.
+		/// The .replace() function removes all special characters from the date.
+		/// This way we can use it as the filename.
+		var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
+
+		console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+
+		NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
+			io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
+			/// The browser will take this new name and load the picture from the public folder.
+		});
+
+		nodemailer.createTestAccount((err, account) => {
+			let transporter = nodemailer.createTransport({
+				host: 'smtp.googlemail.com',
+				port: 465,
+				secure: true,
+				auth: {
+					user: 'ankit.devices123', //Gmail username
+					pass: 'ForIDD$1' // Gmail password
+				}
+			});
+
+			let mailOptions = {
+				from: '"Ankit Doorbell" <ankit.devices123@gmail.com>',
+				to: 'ankitmalhan.iitd@gmail.com', 
+				subject: 'Bro someone is at the door might wanna check that!',
+				text: 'Hey hey bro, someone is at the door might wanna check on that in case you didnt notice',
+				attachments: [{
+					filename: 'image.png',
+					path: '/home/pi/IDD-Fa19-Lab7/public/'+imageName + '.jpg',
+				}]
+			};
+
+			transporter.sendMail(mailOptions, (error, info) => {
+				if (error) {
+					return console.log(error);
+				}
+				console.log('Message sent: %s', info.messageId);
+			});
+		});
+	});
+
+	// if you get the 'disconnect' message, say the user disconnected
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+
+>>>>>>> Minor Tweaks on debugging
 });
 //----------------------------------------------------------------------------//
